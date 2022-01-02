3dccd9d851afcae5eac01012084e1dc2014c68fd
Lucifer25x <74368520+Lucifer25x@users.noreply.github.com>
2022-01-03 03:49:44 +0400

    Update main.js

--- secureMessage/main.js
+++ secureMessage/main.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require('telegraf');
-const config = require('dotenv').config();
+// const config = require('dotenv').config();
 const bot = new Telegraf(process.env.TOKEN);
 let reader;
 let msgText;
